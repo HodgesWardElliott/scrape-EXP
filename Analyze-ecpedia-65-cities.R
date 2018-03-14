477a73f3a160b48cc2520bf1f01287a7470d4d5b
timkiely <rstudio@ip-172-31-47-164.us-west-2.compute.internal>
2018-03-14 19:12:47 +0000

    all data geocoded

--- Analyze-ecpedia-65-cities.R
+++ Analyze-ecpedia-65-cities.R
@@ -1,8 +1,14 @@
+#!/usr/bin/Rscript --default-packages=methods,datasets,utils,grDevices,graphics,stats
 
-library(tidyverse)
-source("~/toClip.R")
+setwd("/home/rstudio/scrape-EXP")
 
-all_hotels <- read_csv('data/EXP-All-hotels.csv') %>% mutate(UID = 1:n())
+if(!"pacman" %in% installed.packages()){
+  install.packages("pacman")
+}
+
+pacman::p_load(tidyverse, googleway)
+
+all_hotels <- suppressWarnings(suppressMessages(read_csv('data/EXP-All-hotels.csv') %>% mutate(UID = 1:n())))
 
 
 # cleanse -----------------------------------------------------------------
@@ -14,7 +20,6 @@ hotel_data <-
 
 
 # Create city rank --------------------------------------------------------
-glimpse(hotel_data)
 
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
@@ -46,18 +51,26 @@ set_key(my_api_key)
 with_addresses <- 
   hotels_ranked %>% 
   filter(!is.na(Address)) %>% 
-  mutate(full_address = paste0(with_addresses$Address, ", ",with_addresses$City_name))
+  mutate(full_address = paste0(Address, ", ",City_name))
 
 
 if(file.exists("data/geocoded_addresses.csv")){
-  already_geocoded <- suppressMessages(read_csv("data/geocoded_addresses.csv"))
+  already_geocoded <- suppressWarnings(suppressMessages(read_csv("data/geocoded_addresses.csv")))
 } else {
   already_geocoded <- tibble()
 }
 
 yet_to_geocode <- anti_join(with_addresses, already_geocoded, by = "UID")
 
+nrow(yet_to_geocode)
+
+if(nrow(yet_to_geocode)==0){
+  message("DONE GEOCODING")
+  Sys.sleep(7200)
+}
+
 for(i in 1:nrow(yet_to_geocode)){
+  on.exit(closeAllConnections())
   message(i," of ",nrow(yet_to_geocode))
   # i <- 1
   the_row <- yet_to_geocode[i,]
@@ -65,11 +78,16 @@ for(i in 1:nrow(yet_to_geocode)){
   status <- FALSE
   try_count <- 0
   while(status!="OK"&try_count<5){
+    Sys.sleep(3)
     message("Geocodeing ",the_address)
-    coded <- google_geocode(the_address, simplify = F)
-    coded_out <- jsonlite::fromJSON(coded, simplifyVector = F)
-    status <- coded_out$status
-    try_count <- try_count+1
+    coded <- suppressWarnings(google_geocode(the_address, simplify = F))
+    
+    if(!is.null(coded)){
+      coded_out <- jsonlite::fromJSON(coded, simplifyVector = F)
+      status <- coded_out$status
+      try_count <- try_count+1
+    } else try_count <- 5
+    
   }
   if(try_count<5){
     message("     ...successful")
